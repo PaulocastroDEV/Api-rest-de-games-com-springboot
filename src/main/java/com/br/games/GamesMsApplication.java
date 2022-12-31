b53b27ba14890e270b4aee58cc61b5af525212ac
Paulo Gustavo <pauloestudando159@gmail.com>
2022-12-31 14:44:35 -0300

    Configuração do mysql

---
+++ src/main/java/com/br/games/GamesMsApplication.java
@@ -0,0 +1,13 @@
+package com.br.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesMsApplication.class, args);
+	}
+
+}
