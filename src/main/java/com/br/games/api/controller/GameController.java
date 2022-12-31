3b81c1f240807d8bcd1cd43d885014e6fae96b11
Paulo Gustavo <pauloestudando159@gmail.com>
2022-12-31 18:01:55 -0300

    Criação do endpoint get para listar apenas um jogo especificado pelo id

--- src/main/java/com/br/games/api/controller/GameController.java
+++ src/main/java/com/br/games/api/controller/GameController.java
@@ -1,8 +1,12 @@
 package com.br.games.api.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +22,13 @@ public class GameController {
 	public List<Game> findAll(){
 		return gameRepository.findAll();
 	}
+	@GetMapping("/{gameid}")
+	public ResponseEntity<Game> findById(@PathVariable Long gameid){
+		Optional<Game> game = gameRepository.findById(gameid);
+		if(game.isPresent()) {
+			return ResponseEntity.ok(game.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
 }
