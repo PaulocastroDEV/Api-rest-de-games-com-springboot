5ec284526560ea050496d4ab5d0fb9ed6f0f4a84
Paulo Gustavo <pauloestudando159@gmail.com>
2023-01-01 10:16:32 -0300

    Criação do endpoint patch para atualizar partes especificas de um game

--- src/main/java/com/br/games/api/controller/GameController.java
+++ src/main/java/com/br/games/api/controller/GameController.java
@@ -1,14 +1,18 @@
 package com.br.games.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,6 +24,7 @@ import com.br.games.domain.exception.EntidadeNaoEncontradaException;
 import com.br.games.domain.model.Game;
 import com.br.games.domain.repository.GameRepository;
 import com.br.games.domain.service.CadastroGameService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/games")
@@ -63,8 +68,29 @@ public class GameController {
 			return ResponseEntity.noContent().build();
 		}catch(EntidadeNaoEncontradaException e) {
 			return ResponseEntity.notFound().build();
+		}		
+	}
+	@PatchMapping("/{gameid}")
+	public ResponseEntity<?> updatePart(@PathVariable Long gameid, @RequestBody Map<String,Object> campos){
+		Optional<Game> gameAtual = gameRepository.findById(gameid);
+		if(gameAtual == null) {
+			return ResponseEntity.notFound().build();
 		}
-
+		merge(campos,gameAtual.get());
+		return update(gameid, gameAtual.get());
+		
+		
+	}
+	
+	private void merge(Map<String,Object> dadosOrigem,Game gameDestino) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Game gameOrigem = objectMapper.convertValue(dadosOrigem, Game.class);
+		dadosOrigem.forEach((nomePropiedade,valorPropriedade)->{
+			Field field = ReflectionUtils.findField(Game.class, nomePropiedade);
+			field.setAccessible(true);
+			Object novoValor = ReflectionUtils.getField(field, gameOrigem);
+			ReflectionUtils.setField(field, gameDestino, novoValor);
+		});
 		
 	}
 }
