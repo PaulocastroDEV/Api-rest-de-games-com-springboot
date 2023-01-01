6a5f999659433517a908c0335fab732cdc7c5fa7
Paulo Gustavo <pauloestudando159@gmail.com>
2023-01-01 09:26:19 -0300

    Criação do endpoint post para adicionar novos games

--- src/main/java/com/br/games/api/controller/GameController.java
+++ src/main/java/com/br/games/api/controller/GameController.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,5 +47,11 @@ public class GameController {
 				return ResponseEntity.ok(gameSalvo);
 			}
 			return ResponseEntity.notFound().build();	
+	}
+	@PostMapping
+	public ResponseEntity<Game> create(@RequestBody Game game){
+		game = cadastroGame.save(game);
+		return ResponseEntity.status(HttpStatus.CREATED).body(game);
+		
 	}
 }
