9fbe311cacd34d455f2c69342209bde4ad66ad22
Paulo Gustavo <pauloestudando159@gmail.com>
2023-01-21 19:41:51 -0300

    Teste do método get para consulta de games

--- src/test/java/com/br/games/GamesMsApplicationTests.java
+++ src/test/java/com/br/games/GamesMsApplicationTests.java
@@ -1,13 +1,44 @@
 package com.br.games;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import com.br.games.domain.repository.GameRepository;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GamesMsApplicationTests {
-
+	
+	@LocalServerPort
+	private int port;
+	
+	
+	@Autowired
+	private GameRepository gameRepository;
+	@BeforeEach
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port=port;
+		RestAssured.basePath="/games";
+	}
 	@Test
-	void contextLoads() {
+	public void MustReturnStatus200() {
+		RestAssured
+		.given()
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
 	}
 
 }
